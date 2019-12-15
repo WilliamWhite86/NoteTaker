bd8cb76d2720641e4958129898a21917231d0367
William White <william.white86@gmail.com>
2019-12-15 12:38:22 -0500

    added deletes writing to db file, changed how id numbers assigned to ensure unique ids

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -14,10 +14,6 @@ const path = require("path");
 module.exports = function (app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
 
 
   app.get("/api/notes", function (req, res) {
@@ -30,26 +26,25 @@ module.exports = function (app) {
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
   app.post("/api/notes", function (req, res) {
     fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
       if (err) throw err;
       let newNote = req.body;
-      let id = Math.floor(Math.random() * 1000);  
+      //let id = Math.floor(Math.random() * 1000);
       let notesArr = (JSON.parse(data));
-      newArr.id = id;
+      let id = notesArr[notesArr.length - 1].id + 1;
+      newNote.id = id;
       notesArr.push(newNote);
-        //req.body + `{"id":"${id}"}`);
+      //req.body + `{"id":"${id}"}`);
       let notesString = JSON.stringify(notesArr);
       console.log(typeof notesString);
       fs.writeFileSync(path.join(__dirname, "../db/db.json"), notesString)
-    });      
+    });
   });
+
+  // API DELETE Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
 
   app.delete("/api/notes/:id", function (req, res) {
     fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
@@ -58,11 +53,15 @@ module.exports = function (app) {
       //console.log(JSON.parse(data));
       let notesArr = (JSON.parse(data));
       let newNotesArr = []
-      for (i = 0; i < notesArr.length; i++){
-        if (notesArr[i].id !== req.params.id){
+      for (i = 0; i < notesArr.length; i++) {
+        if (notesArr[i].id != req.params.id) {
           newNotesArr.push(notesArr[i]);
         }
-    }
-    console.log(newNotesArr);
+      }
+      console.log(newNotesArr);
+      let notesString = JSON.stringify(newNotesArr);
+      console.log(notesString);
+      fs.writeFileSync(path.join(__dirname, "../db/db.json"), notesString)
+    })
   })
-})};
+};

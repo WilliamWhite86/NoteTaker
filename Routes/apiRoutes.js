3d0fa1582abf8bfb37bc35eba4fd7405f099a06e
William White <william.white86@gmail.com>
2019-12-15 08:50:40 -0500

    got posts and gets working using JSON.parse and JSON.stringify, reaching out to db.json

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -4,36 +4,52 @@
 // These data sources hold arrays of information on notes.
 // ===============================================================================
 
-var notesData = require("../data/notes");
+const fs = require("fs");
+const path = require("path");
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
-  
-    app.get("/api/notes", function(req, res) {
-      res.json(notesData);
+module.exports = function (app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+
+
+  app.get("/api/notes", function (req, res) {
+    fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
+      if (err) throw err;
+      console.log(JSON.parse(data));
+      res.json(JSON.parse(data));
     });
-  
-    // API POST Requests
-    // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
-    // ---------------------------------------------------------------------------
-  
-    app.post("/api/notes", function(req, res) {
-      // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-      // It will do this by sending out the value "true" have a table
-      // req.body is available since we're using the body parsing middleware
-        notesData.push(req.body);
-    });
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/notes", function (req, res) {
+    fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
+      if (err) throw err;
+      let notesObj = (JSON.parse(data));
+      notesObj.push(req.body);
+      let notesString = JSON.stringify(notesObj);
+      console.log(typeof notesString);
+      fs.writeFileSync(path.join(__dirname, "../db/db.json"), notesString)
+      
+
+    });      
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // req.body is available since we're using the body parsing middleware
+    //console.log(req.body)
+    //notesData.push(req.body);
+  });
 };
-  
--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 
-var express = require("express");
+const express = require("express");
+const fs = require("fs");
+const app = express();
 
-var app = express();
+const PORT = process.env.PORT || 8080;
 
-var PORT = process.env.PORT || 8080;
+// stringify
+
+// parse, push, stringify
+
+app.use(express.static("public"))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./Routes/apiRoutes")(app);
+require("./Routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
